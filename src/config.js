ff9a61bf86ddfac7a0fc13a9948f9ef417492d56
Daniel Machado <danielmachadopintos@gmail.com>
2018-08-13 13:06:52 -0300

    insirindo os novos arquivos, para o webhoock

---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  useNewUrlParser: true,
+  dbUrl:"mongodb://admin:admin123@ds013014.mlab.com:13014/nba"
+};
---
+++ src/index.js
@@ -0,0 +1,34 @@
+'use strict';
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  http = require('http'),
+  config = require('./config'),
+  server = express(),
+  mongoose = require('mongoose'),
+  TeamInfo = require('./Models/TeamInfo'), //created model loading here
+  GameSchedule = require('./Models/GameSchedule');
+  var routes = require('./routes/Routes'); //importing route
+
+const port = normalizePort(process.env.PORT || '3000');
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect(config.dbUrl);
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+routes(server); //register the route
+server.listen((port), function () {
+  console.log("Server is up and listening on port: [" + port + "]");
+});
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
